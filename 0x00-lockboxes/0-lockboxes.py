5365654b018ccff4ba3c7306e2de064042aab359
Victor Hernandez <vik407@gmail.com>
2020-07-20 23:46:44 -0500

    Corrections and pep8 validation

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
+""" You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 """
-"""
+import json
+
+
 def canUnlockAll(boxes):
+    """ Write a method that determines if all the boxes can be opened.
+    @boxes is a list of lists
     """
-    """
-    # Basic requirements
+    # Basic checks
     if not boxes:
         return False
     if type(boxes) is not list:
         return False
-    for box in boxes:
-        if type(box) is not list:
-            return False
 
-    founded_keys = [key for key in boxes[0]]
-    # Get all the keys
-    for box in boxes:
-        if len(founded_keys) == 0:
-            return False
-        for key in box:
-            temp_key = []
-            if key < len(boxes):
-                for new_key in boxes[key]:
-                    temp_key.append(new_key)
-            for new_key in temp_key:
-                # Dont repeat founded keys
-                if new_key not in founded_keys:
-                    founded_keys.append(new_key)
-    print("Keys to test {}".format(founded_keys))
-    print("Boxes {}".format(boxes))
-    for try_open in boxes:
-        print("try open {} with {}".format(try_open, founded_keys))
-        if len(founded_keys) == len(try_open):
-            print("open")
-            return True
+    unlocked = {'0': True}
+    # Get first box unlocked
+    for key in boxes[0]:
+        unlocked_key = '{}'.format(key)
+        unlocked[unlocked_key] = True
+
+    # Unlock them
+    for unlocked_key in list(unlocked.keys()):
+        key_pos = int(unlocked_key)
+        for key in boxes[key_pos]:
+            deeper_unlocked = '{}'.format(key)
+            unlocked[deeper_unlocked] = True
+
+    # Move through boxes
+    pos = 1
+    for box in boxes[1:]:
+        if str(pos) in list(unlocked.keys()):
+            for key in box:
+                unlocked['{}'.format(key)] = True
+        pos += 1
+
+    if len(boxes) > len(unlocked.keys()):
         return False
+    else:
+        return True
