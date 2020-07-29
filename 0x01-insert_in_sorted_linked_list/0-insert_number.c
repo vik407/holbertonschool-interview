e7cacb1aa45a3b90da4e419be068ed05786d619d
Victor Hernandez <vik407@gmail.com>
2020-07-28 22:23:54 -0500

    Edge case

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,7 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
 /**
  * insert_node - function to insert a number into a sorted singly linked list
--- 0x01-insert_in_sorted_linked_list/lists.h
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +15,8 @@
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
