6661a3fbb94dcb69ffc31be57bfc1a3dd828ab1f
Victor Hernandez <vik407@gmail.com>
2020-08-04 21:52:56 -0500

    Working...

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -14,7 +14,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
@@ -26,7 +26,6 @@ struct binary_tree_s
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
