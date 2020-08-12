03e872f8c62f8273ca4a11d7360df70b3758e3fa
Victor Hernandez <vik407@gmail.com>
2020-08-11 20:45:53 -0500

    Doing recursively

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-"""
-0. Minimum Operations
-"""
+
+
+def minOperations(n):
+    """
+    0. Minimum Operations
+    """
+    if n <= 1:
+        return 0
+    for op in range(2, n + 1):
+        if n % op == 0:
+            return minOperations(int(n / op)) + op
