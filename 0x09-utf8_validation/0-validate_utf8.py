70413d783cbaf59aa3b0461eacdedadad28d64c2
Victor Hernandez <vik407@gmail.com>
2020-09-23 23:26:07 -0500

    identation fix

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -12,12 +12,12 @@ def validUTF8(data):
             while mask & n:
                 num_bytes += 1
                 mask >>= 1
-            if not num_bytes:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
-                return False
-        else:
-            if n >> 6 != 0b10:
-                return False
-        num_bytes -= 1
-    return num_bytes == 0
+                if not num_bytes:
+                    continue
+                if num_bytes == 1 or num_bytes > 4:
+                    return False
+            else:
+                if n >> 6 != 0b10:
+                    return False
+            num_bytes -= 1
+        return num_bytes == 0
