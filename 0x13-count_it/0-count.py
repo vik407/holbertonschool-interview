8d8ce470eb437e459b3caf9df1dbf0be8d564b57
Victor Hernandez <vik407@gmail.com>
2020-12-18 15:54:19 -0500

    Counting

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" 0. Count it!
+"""
+
+from collections import Counter, defaultdict
+
+import re
+import requests
+
+
+def count_words(subreddit, word_list, res=defaultdict(int), after=None):
+    """ Looping in subreddit """
+    agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97\
+            Safari/537.36"
+    headers = {"User-Agent": agent}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    if after:
+        url += '?after={}'.format(after)
+    try:
+        r = requests.get(url, headers=headers, allow_redirects=False).json()
+        titles = r.get('data').get('children')
+        for t in titles:
+            c = Counter(t.get('data').get('title').lower().split(' '))
+            for word in word_list:
+                if word.lower() in c:
+                    res[word] += c.get(word.lower())
+        after = r.get('data').get('after')
+        if after:
+            return count_words(subreddit, word_list, res, after)
+        first_sort = sorted(res.items(), key=lambda x: x[0])
+        for k, v in sorted(first_sort, key=lambda x: x[1], reverse=True):
+            print('{}: {}'.format(k, v))
+    except:
+        return
---
+++ 0x13-count_it/README.md
@@ -0,0 +1,19 @@
+# 0x13. Count it!
+
+## Resources:books:
+Read or watch:
+* [Reddit API Documentation](https://intranet.hbtn.io/rltoken/PV_GanilbTliu3BSqFKPKA)
+
+---
+## Learning Objectives:bulb:
+What you should learn from this project:
+
+---
+
+### [0. Count it!](./0-count.py)
+* Write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not).
+
+---
+
+## Author
+* **Victor Hernandez** - [vik407](https://github.com/vik407)
