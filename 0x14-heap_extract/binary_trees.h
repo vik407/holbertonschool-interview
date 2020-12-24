d03e14b3067c2d482ce740b11f99e0da9de4d8aa
Victor Hernandez <vik407@gmail.com>
2020-12-23 22:47:04 -0500

    heap extract

---
+++ 0x14-heap_extract/binary_trees.h
@@ -0,0 +1,35 @@
+#ifndef BIN_TREE
+#define BIN_TREE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+size_t binary_tree_size(const binary_tree_t *tree);
+int heap_extract(heap_t **root);
+int free_last(heap_t **root);
+void xor_swap(int *a, int *b);
+void bubble_down(heap_t *node);
+
+#endif
